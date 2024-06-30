57300d92d78254933f80a64c958d79ffe2bf625d
Akuta Zehy <akuta_zehy@outlook.com>
2024-06-30 20:48:04 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,7 +3,11 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** 
+     * The Collatz conjecture is a conjecture in mathematics that concerns a sequence defined as follows: start with any positive integer n. Then each term is obtained from the previous term as follows: if the previous term is even, the next term is one half of the previous term. If the previous term is odd, the next term is 3 times the previous term plus 1. The conjecture is that no matter what value of n, the sequence will always eventually reach 1.
+     * 
+     * @source https://en.wikipedia.org/wiki/Collatz_conjecture
+     */
     public static int nextNumber(int n) {
         if (n % 2 == 0) {
             return n / 2;
